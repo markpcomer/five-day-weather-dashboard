6c4aa6a3a77c39b8dc48c275fe02b1faaa4345dd
markpcomer <markpcomer@gmail.com>
2024-03-05 14:38:33 -0500

    Further HTML and beginning JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,31 @@
+/*
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+*/
+
+var APIKey = "cbc00e00ddc9ea240d4e40b6ebc3d6f6";
+var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+var cities = [];
+var q = "";
+
+function displayCurrentWeather(city, weather){
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    
+  })
+}
 
 
 
