f433143112c8abf22b322686b260cb96dcb9ed84
markpcomer <markpcomer@gmail.com>
2024-06-01 15:59:06 -0400

    added fetch weather data & render current weather funcs

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,6 @@
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-*/
 
 var APIKey = "cbc00e00ddc9ea240d4e40b6ebc3d6f6";
-var newAPIKey = "20ef3ea6758a437fd090faae1ef08152";
+var newAPIKey= "20ef3ea6758a437fd090faae1ef08152";
 var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={newAPIKey}`;
 var cities = [];
 var imperialQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API key}&units=imperial";
@@ -23,7 +12,79 @@ var searchHistory = document.querySelector('#search-history');
 var forecastCards = document.querySelector('#future-cards');
 var cityName = document.querySelector('#city-name');
 
+var currentWeather = document.querySelector('#current-weather');
 
+
+function fetchWeatherData(city) {
+  var { lat } = location;
+  var { lon } = location;
+  var city = city.name;
+  
+  var imperialQueryURL = `https://api.openweathermap.org/data/2.5/forecast?
+    lat=${lat}&lon=${lon}&appid=${newAPIKey}&units=imperial`;
+
+
+    return fetch(imperialQueryURL)
+      .then(function (res) {
+        if(!res) {
+          throw new Error("Response not okay");
+        }
+        return res.json();
+      });
+}
+
+function renderCurrentWeatherCard(data) {
+  var todayDate = dayjs().format('M/D/YYYY');
+  var city = data.name;
+  var temp = data.main.temp;
+  var humidity = data.main.humidity;
+  var wind = data.wind.speed;
+
+  // Card
+  var resultCard = document.createElement('div');
+  resultCard.setAttribute('class', 'result-card');
+  resultCard.append(resultCardBody);
+
+  // Card title
+  var resultCardTitle = document.createElement('h3');
+  resultCardTitle.setAttribute('class', 'h3 result-card-title');
+  resultCardTitle.textContent = `${city} ${todayDate}`;
+
+  // Card body
+  var resultCardBody = document.createElement('div');
+  resultCardBody.setAttribute('class', 'result-card-body');
+  cardBody.append(resultCardTitle, resultTemp, resultHumidity, resultWind);
+
+  // Temp
+  var resultTemp = document.createElement('p');
+  resultTemp.setAttribute('class', 'result-temp');
+  resultTemp.textContent = `${temp}`;
+
+  // Humidity
+  var resultHumidity = document.createElement('p');
+  resultHumidity.setAttribute('class', 'result-humidity');
+  resultHumidity.textContent = `${humidity}`;
+
+  // Wind
+  var resultWind = document.createElement('p');
+  resultWind.setAttribute('class', 'result-wind');
+  resultWind.textContent = `${wind}`;
+
+  currentWeather.empty();
+  currentWeather.append(resultCard);
+
+}
+
+function displayCurrentWeather(city) {
+  fetchWeatherData(city)
+    .then(renderCurrentWeatherCard)
+    .catch(function (err) {
+      console.log("Error", err);
+    })
+}
+
+
+/* FIRST ATTEMPT
 function fetchWeather(city) {
   var imperialQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={API key}&units=imperial";
   fetch(imperialQueryURL)
@@ -33,100 +94,18 @@ function fetchWeather(city) {
     .then (function (data) {
       displayCityWeather(data, city);
     })
-}
+    console.log("");
+};
 
+function fetchLocation(lat, lon) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + newAPIKey;
 
-
-
-
-
-
-
-
-// Reads projects from local storage and returns array of project objects.
-// Returns an empty array ([]) if there aren't any projects.
-/*
-function readProjectsFromStorage() {
-  var projects = localStorage.getItem('projects');
-  if (projects) {
-    projects = JSON.parse(projects);
-  } else {
-    projects = [];
-  }
-  return projects;
-}
-
-// Takes an array of projects and saves them in localStorage.
-function saveProjectsToStorage(projects) {
-  localStorage.setItem('projects', JSON.stringify(projects));
-}
-
-// Gets project data from local storage and displays it
-function printProjectData() {
-  // clear current projects on the page
-  projectDisplayEl.empty();
-
-  // get projects from localStorage
-  var projects = readProjectsFromStorage();
-
-  // loop through each project and create a row
-  for (var i = 0; i < projects.length; i += 1) {
-    var project = projects[i];
-    var projectDate = dayjs(project.date);
-    // get date/time for start of today
-    var today = dayjs().startOf('day');
-
-    // Create row and columns for project
-    var rowEl = $('<tr>');
-    var nameEL = $('<td>').text(project.name);
-    var typeEl = $('<td>').text(project.type);
-    var dateEl = $('<td>').text(projectDate.format('MM/DD/YYYY'));
-
-    // Save the index of the project as a data-* attribute on the button. This
-    // will be used when removing the project from the array.
-    var deleteEl = $(
-      '<td><button class="btn btn-sm btn-delete-project" data-index="' +
-        i +
-        '">X</button></td>'
-    );
-
-    // add class to row by comparing project date to today's date
-    if (projectDate.isBefore(today)) {
-      rowEl.addClass('project-late');
-    } else if (projectDate.isSame(today)) {
-      rowEl.addClass('project-today');
-    }
-
-    // append elements to DOM to display them
-    rowEl.append(nameEL, typeEl, dateEl, deleteEl);
-    projectDisplayEl.append(rowEl);
-  }
-}
-function handleProjectFormSubmit(event) {
-  event.preventDefault();
-
-  // read user input from the form
-  var projectName = projectNameInputEl.val().trim();
-  var projectType = projectTypeInputEl.val(); // don't need to trim select input
-  var projectDate = projectDateInputEl.val(); // yyyy-mm-dd format
-
-  var newProject = {
-    name: projectName,
-    type: projectType,
-    date: projectDate,
-  };
-
-  // add project to local storage
-  var projects = readProjectsFromStorage();
-  projects.push(newProject);
-  saveProjectsToStorage(projects);
-
-  // print project data
-  printProjectData();
-
-  // clear the form inputs
-  projectNameInputEl.val('');
-  projectTypeInputEl.val('');
-  projectDateInputEl.val('');
+  fetch(queryURL)
+    .then(function (response){
+      return response.json();
+    })
+    .then (function (lat, lon){
+      
+    })
 }
 */
